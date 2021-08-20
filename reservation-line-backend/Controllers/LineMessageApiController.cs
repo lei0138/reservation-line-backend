58eee5fb4b21e62c51e1e354431e7d551c7764ad
Steven Chen <stevenchen0138@gmail.com>
2021-08-20 17:17:59 +0800

    update routing to 'linemessageapi'

--- reservation-line-backend/Controllers/LineMessageApiController.cs
+++ reservation-line-backend/Controllers/LineMessageApiController.cs
@@ -9,16 +9,16 @@ namespace reservation_line_backend.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class LineMessageApiController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<LineMessageApiController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public LineMessageApiController(ILogger<LineMessageApiController> logger)
         {
             _logger = logger;
         }
